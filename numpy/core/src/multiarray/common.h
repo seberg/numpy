494b1b4318a8d1cee59ce062bdfc5840460e4a67
Sebastian Berg <sebastian@sipsolutions.net>
2019-10-14 17:45:07 -0700

    MAINT,(DTYPE): Delete old dtype discovery code

--- numpy/core/src/multiarray/common.h
+++ numpy/core/src/multiarray/common.h
@@ -20,28 +20,6 @@
 #endif
 
 /*
- * Recursively examines the object to determine an appropriate dtype
- * to use for converting to an ndarray.
- *
- * 'obj' is the object to be converted to an ndarray.
- *
- * 'maxdims' is the maximum recursion depth.
- *
- * 'out_dtype' should be either NULL or a minimal starting dtype when
- * the function is called. It is updated with the results of type
- * promotion. This dtype does not get updated when processing NA objects.
- *
- * Returns 0 on success, -1 on failure.
- */
-NPY_NO_EXPORT int
-PyArray_DTypeFromObject(PyObject *obj, int maxdims,
-                        PyArray_Descr **out_dtype);
-
-NPY_NO_EXPORT int
-PyArray_DTypeFromObjectHelper(PyObject *obj, int maxdims,
-                              PyArray_Descr **out_dtype, int string_status);
-
-/*
  * Returns NULL without setting an exception if no scalar is matched, a
  * new dtype reference otherwise.
  */
