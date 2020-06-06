5497b92f572f0d49c4c520c98131a27602370691
Sebastian Berg <sebastian@sipsolutions.net>
2020-06-06 15:38:57 -0500

    rewrite braodcast/axisdata filling in nditer
    Only remaining issue is that I currently store the shape directly,
    but the error reporting expects broadcast shape to be filled...

--- numpy/core/src/multiarray/nditer_impl.h
+++ numpy/core/src/multiarray/nditer_impl.h
@@ -133,6 +133,8 @@
 #define NPY_OP_ITFLAG_FORCECOPY    0x0200
 /* The operand has temporary data, write it back at dealloc */
 #define NPY_OP_ITFLAG_HAS_WRITEBACK 0x0400
+/* The operand requires reduction, but cannot be reduced (construction only) */
+#define NPY_OP_ITFLAG_NO_REDUCE 0x0800
 
 /*
  * The data layout of the iterator is fully specified by
